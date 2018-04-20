9532f67f3737f5613009a5ba86b670b3b4942919
jhover <jhover@starkhorn.rhic.bnl.gov>
2018-04-20 11:04:52 -0400

    Update.

--- bin/initialize-template.sh
+++ bin/initialize-template.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo "Cloning current catalog..."
 cd /etc/puppet
+
 git clone https://webdocs.racf.bnl.gov/git/puppet/catalog
+
+if [ $? -eq 128 ]
+then
+   echo "Catalog exists already, updating..."
+   cd catalog
+   git pull
+fi
 
 echo "Adjusting puppet.conf..."
 cp -f /etc/puppet/puppet.conf /etc/puppet/puppet.conf.backup
